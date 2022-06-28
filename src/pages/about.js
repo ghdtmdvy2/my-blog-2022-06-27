3108a5b5eaf8fa1a9f9095721fafb3e2991b2d8d
ghdtmdvy2 <ghdtmdvy2@gmail.com>
2022-06-28 17:06:23 +0900

    오류발생

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,14 @@
 import React from "react";
-import Layout from "./components/Layout";
+import Layout from "../components/Layout";
 
 function AboutPage() {
   return (
-    <Layout>
-      <title>자기소개</title>
-      <h1>About page</h1>
+    <Layout pageTitle="About Me">
+      <p>
+        Hi there! I'm the proud creator of this site, which I built with Gatsby.
+      </p>
     </Layout>
   );
 }
+
 export default AboutPage;
